dd6d45779d6bb406561fca4b7602ff3da2b9196e
JFarah <joaquinefarah@gmail.com>
2024-05-09 17:00:53 -0300

    Primer commit

---
+++ app/comments/data.ts
@@ -0,0 +1,22 @@
+export const comments = [
+    {
+        id: 1,
+        text: "Came for lunch with my sister. We loved our Thai-style mains which were amazing with lots of flavour, very impressive for a vegetarian restaurant. But the service was below average and the chips were too terrible to finish. When we arrived at 1.40, we had to wait 20 minutes while they got our table ready. OK, so we didn't have a reservation, but the restaurant was only half full. There was no reason to make us wait at all. We ordered the chips as a side dish and they looked delicious. But, when we tasted them, they were overcooked and swimming in oil so we left most of them. We expected a lot more for $10! When the waiter asked if everything was ok, we said we really didn't like the chips and he said 'That's funny, I love them' and that was it. He didn't offer us anything else or take them off our bill. Also, when we didn't leave a tip, he looked annoyed. I was really excited about visiting Vega, and the mains were just fantastic, but the rest of the experience was really disappointing.",
+    },
+    {
+        id: 2,
+        text: "Restaurant review wanted. Have you been to a restaurant recently that you either loved or hated? Write us a review explaining your opinions. Tell us about specific elements of the restaurant which caught your attention. The best reviews will be published in the magazine.",
+    },
+    {
+        id: 3,
+        text: "As you enter the place you are welcomed by a magnificent setting, a delightful marriage of antique cut stones and the luxuries of modernity.  Sitting in any table you have a wonderful view of the workshop-like kitchen where you can see the chefs working. For those who do not book a table and are waiting, there is a cosy lounge with aged leather armchairs and pictures of polo players displayed on the walls. ",
+    },
+    {
+        id: 4,
+        text: "The menu offers a wide variety of mouth-watering starters. I had the white asparagus accompanied by a mousseline sauce and poached eggs, while my friend tried the zucchini carpaccio served with sundries tomatoes and a mozzarella strudel. They were exquisite. The main course consisted in a risotto with scallop cooked in cream and a cassolette of coley with mussels, which really thrilled us. It all was followed by a delicious apple pie served with a scoop of vanilla ice cream for dessert. ",
+    },
+    {
+        id: 5,
+        text: "All the products were fresh and the dishes had the warmth of home-made food. The chefs takes great care in selecting the best quality ingredients. The restaurant also caters for all tastes by providing a large selection of the best wines of the region, which are pricy but worth sampling.",
+    },
+];
