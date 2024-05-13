5720518488a0f3decdd48eeb7724a69025d02c71
JFarah <joaquinefarah@gmail.com>
2024-05-13 11:19:56 -0300

    minor fix

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -1,7 +1,7 @@
-import { comments } from "../data";
+import { commentaries } from "../data";
 
 export async function GET(_request: Request, {params}: {params: {id:string} }) {
-    const comment = comments.find((comment) => comment.id === parseInt(params.id));
+    const comment = commentaries.find((comment) => comment.id === parseInt(params.id));
     return Response.json(comment);
 } 
 
